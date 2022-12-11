1a7e2ac44a23e40ff3df2244202dc7b76a1fa92e
Marcia Oliveira <99289503+MarciaGabrielle@users.noreply.github.com>
2022-12-11 16:49:36 -0400

    Create auxilioPrint.java

---
+++ SupermercadoProjeto/src/supermercadoprojeto/auxilioPrint.java
@@ -0,0 +1,27 @@
+package projetosupermercado;
+
+import java.util.Scanner;
+
+public class auxilioPrint {
+    private static Scanner in = new Scanner(System.in);
+    
+    //METODOS APENAS PARA AUXILIAR NAS IMPRESSOES DE MENSAGENS
+    public static void ImprimaMensagem(String mensagem){
+        System.out.println("*****************************************************************");
+        System.out.println(mensagem); 
+        System.out.println("*****************************************************************");
+    }
+    
+    public static void ImprimaMensagem(String mensagem1, String mensagem2){
+        System.out.println("*****************************************************************");
+        System.out.println(mensagem1); 
+        System.out.println(mensagem2); 
+        System.out.println("*****************************************************************");
+    }
+    
+    public static void Continuar(){
+        System.out.println();
+        System.out.println("Aperte ENTER para continuar ...");
+        in.nextLine();
+    }
+}
