184a9b397b00a5324fce599cbd1d0ec66cb92ad7
Gabriel Peixoto Menezes da Costa <100876842+gabiel98@users.noreply.github.com>
2022-12-11 16:45:09 -0400

    Create Funcionario.java

---
+++ SupermercadoProjeto/src/supermercadoprojeto/Funcionario.java
@@ -0,0 +1,17 @@
+package projetosupermercado;
+
+public abstract class Funcionario {
+    
+//ATRIBUTOS---------------------------------------------------------------------
+    
+    private String nome;
+    private String userName;
+    private String senha;
+
+//CONSTRUTOR--------------------------------------------------------------------
+    
+    public Funcionario(String nome, String userName, String senha) {
+        this.nome = nome;
+        this.userName = userName;
+        this.senha = senha;
+    }
