21e7cd2e8e1db7b29a4bcc9ea1223a44506cd5f0
Gabriel Peixoto Menezes da Costa <100876842+gabiel98@users.noreply.github.com>
2022-12-11 16:57:45 -0400

    Create Registradora.java

---
+++ SupermercadoProjeto/src/supermercadoprojeto/Registradora.java
@@ -0,0 +1,39 @@
+package projetosupermercado;
+
+public class Registradora {
+    EstoqueProduto estoque;
+    Venda v;
+    
+    
+    public Registradora() {    
+        estoque = new EstoqueProduto();
+    }     
+    
+    public void iniciarVenda(){
+        v = new Venda(); //instanciando um objeto venda
+                        //vamos guardar esse objeto como atributo
+        estoque = new EstoqueProduto();
+    }
+    
+    //mostra o valor total da compra
+    public void concluirVenda(){
+        System.out.println("Total: R$"+v.valorTotal());
+    }
+    
+    //Adicionar mais produtos 
+    public void incluirProduto(int id, int qtd){
+        Produto produto = estoque.getEspecificacao(id);
+        v.incluirItemDeVenda(produto, qtd);
+    }
+    
+    //efetua o pagamento 
+    public void pagamento(float valorPago){
+        v.efetuarPagamento(valorPago);
+    }
+    
+    //devolve o valor do troco
+    public void getTroco(){
+        System.out.println("Troco: R$" + v.getTroco());
+    }
+    
+}
