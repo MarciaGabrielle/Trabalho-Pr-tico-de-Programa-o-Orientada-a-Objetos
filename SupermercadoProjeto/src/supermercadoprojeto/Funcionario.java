70c33e5887f83f9ff5c7f650182c022f2e9544fd
Gabriel Peixoto Menezes da Costa <100876842+gabiel98@users.noreply.github.com>
2022-12-11 16:54:24 -0400

    Update Funcionario.java

--- SupermercadoProjeto/src/supermercadoprojeto/Funcionario.java
+++ SupermercadoProjeto/src/supermercadoprojeto/Funcionario.java
@@ -15,3 +15,35 @@ public abstract class Funcionario {
         this.userName = userName;
         this.senha = senha;
     }
+//METODOS ESPECIAIS-------------------------------------------------------------
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+//METODOS-----------------------------------------------------------------------
+
+    public abstract void login(OperadorDeCaixa operador, int tentativas, int chances);
+    
+    
+}
